3b0c98a1c3e693be4b8f9358e17ae6647478a095
titoeb <tim.toebrock@gmail.com>
2021-12-29 15:59:07 +0000

    Improve tests for utils.
    I improved the tests for the utils. I
    - Move test for function in utils from `solution.rs` to `utils.rs`.
    - Add more tests for `remove_elem`and `change_order`
    - Also changed `remove_elem` and `change_order` logic for bugs found by adding test.

--- src/utils.rs
+++ src/utils.rs
@@ -8,13 +8,23 @@ pub fn get_elem_from_range<T>(range: Range<T>) -> T
 where
     T: std::cmp::PartialOrd + rand::distributions::uniform::SampleUniform,
 {
-    rand::thread_rng().gen_range::<T, Range<T>>(range)
+    if !range.is_empty() {
+        rand::thread_rng().gen_range::<T, Range<T>>(range)
+    } else {
+        range.start
+    }
 }
 pub fn change_order(data: &[usize], put_before_idx: usize, move_idx: usize) -> Vec<usize> {
     let mut new_data = data.to_owned();
-    let move_item = data[move_idx];
-    new_data.remove(move_idx);
-    new_data.insert(max(put_before_idx, 1) - 1, move_item);
+    if put_before_idx != move_idx {
+        let move_item = data[move_idx];
+        new_data.remove(move_idx);
+        let reset_index = (move_idx < put_before_idx) as usize;
+        new_data.insert(
+            max(put_before_idx, reset_index as usize) - reset_index as usize,
+            move_item,
+        );
+    }
     new_data
 }
 
@@ -29,4 +39,107 @@ pub fn ordered_crossover(
     subsequence: Subsequence,
 ) -> Solution {
     unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    mod get_elem_from_range {
+        use super::*;
+        #[test]
+        fn sample_int_range() {
+            get_elem_from_range(0..10);
+        }
+        #[test]
+        fn sample_float_range() {
+            get_elem_from_range(0.0..1.0);
+        }
+        #[test]
+        fn sample_empty_range() {
+            assert_eq!(get_elem_from_range(0..0), 0);
+        }
+    }
+    mod test_remove_elem {
+        use super::*;
+        #[test]
+        fn remove_first() {
+            assert_eq!(remove_elem(vec![1, 2, 3, 4], &0), vec![2, 3, 4]);
+        }
+        #[test]
+        fn remove_last() {
+            assert_eq!(remove_elem(vec![1, 2, 3, 4], &3), vec![1, 2, 3]);
+        }
+        #[test]
+        fn remove_middle() {
+            assert_eq!(remove_elem(vec![1, 2, 3, 4], &2), vec![1, 2, 4]);
+        }
+        #[test]
+        fn test_remove_elem_first() {
+            assert_eq!(remove_elem(vec![1, 2, 3], &0), vec![2, 3])
+        }
+        #[test]
+        fn test_remove_elem_middle() {
+            assert_eq!(remove_elem(vec![1, 2, 3], &1), vec![1, 3])
+        }
+        #[test]
+        fn test_remove_elem_last() {
+            assert_eq!(remove_elem(vec![1, 2, 3], &2), vec![1, 2])
+        }
+    }
+    mod test_change_elem {
+        use super::*;
+        #[test]
+        fn put_before_first() {
+            assert_eq!(change_order(&vec![1, 2, 3, 4], 0, 1), vec![2, 1, 3, 4]);
+        }
+        #[test]
+        fn put_last_before_first() {
+            assert_eq!(change_order(&vec![1, 2, 3, 4], 0, 3), vec![4, 1, 2, 3]);
+        }
+        #[test]
+        fn put_first_before_second() {
+            assert_eq!(change_order(&vec![1, 2, 3, 4], 1, 0), vec![1, 2, 3, 4]);
+        }
+        #[test]
+        fn put_before_second() {
+            assert_eq!(change_order(&vec![1, 2, 3, 4], 1, 2), vec![1, 3, 2, 4]);
+        }
+        #[test]
+        fn put_last_before_second() {
+            assert_eq!(change_order(&vec![1, 2, 3, 4], 1, 3), vec![1, 4, 2, 3]);
+        }
+        #[test]
+        fn put_first_before_last() {
+            assert_eq!(change_order(&vec![1, 2, 3, 4], 3, 0), vec![2, 3, 1, 4]);
+        }
+        #[test]
+        fn put_fourth_before_fourth() {
+            assert_eq!(change_order(&vec![1, 2, 3, 4], 3, 3), vec![1, 2, 3, 4]);
+        }
+        #[test]
+        fn put_first_before_first() {
+            assert_eq!(change_order(&vec![1, 2, 3, 4], 3, 3), vec![1, 2, 3, 4]);
+        }
+        #[test]
+        fn test_change_order_move_first() {
+            assert_eq!(change_order(&vec![1, 2, 3], 1, 0), vec![1, 2, 3])
+        }
+        #[test]
+        fn test_change_order_move_middle() {
+            assert_eq!(change_order(&vec![1, 2, 3], 0, 1), vec![2, 1, 3])
+        }
+
+        #[test]
+        fn test_change_order_move_last() {
+            assert_eq!(change_order(&vec![1, 2, 3], 0, 2), vec![3, 1, 2])
+        }
+        #[test]
+        fn test_change_order_move_first_before_last() {
+            assert_eq!(change_order(&vec![1, 2, 3], 2, 0), vec![2, 1, 3])
+        }
+        #[test]
+        fn test_change_order_move_middle_before_last() {
+            assert_eq!(change_order(&vec![1, 2, 3], 2, 1), vec![1, 2, 3])
+        }
+    }
 }
