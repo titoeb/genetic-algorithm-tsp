8e32f2ee953441d0b486575d05433d7b4867495e
titoeb <tim.toebrock@gmail.com>
2022-01-07 07:59:30 +0000

    Add a time benchmark to the crate.

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use genetic_algo::distance_mat::DistanceMat;
+use genetic_algo::population::{evolve_population, Population};
+use std::fs;
+use std::time::Instant;
+
+fn benchmark_population(
+    n_generations: usize,
+    size_generation: usize,
+    dist_mat: &DistanceMat,
+) -> u64 {
+    // End-to-end test: does the error of the solution get down?
+    let before = Instant::now();
+    evolve_population(
+        Population::random(size_generation, dist_mat.n_units()),
+        n_generations,
+        size_generation,
+        dist_mat,
+    );
+    let duration = before.elapsed();
+    let nanos = duration.subsec_nanos() as u64;
+    (1000 * 1000 * 1000 * duration.as_secs() + nanos) / (1000 * 1000)
+}
+fn main() {
+    let distances = DistanceMat::new(
+        fs::read_to_string("tests/test-data/distances.txt")
+            .unwrap()
+            .lines()
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|line| {
+                line.split(';')
+                    .map(|float_string| float_string.parse::<f64>().unwrap())
+                    .collect::<Vec<f64>>()
+            })
+            .collect(),
+    );
+    for n_generations in (10..=510).step_by(100) {
+        for size_generation in (10..=40).step_by(10) {
+            let run_time = benchmark_population(n_generations, size_generation, &distances);
+            println!(
+                "n_generations: {}, size_generation: {}, time: {} ms",
+                n_generations, size_generation, run_time
+            );
+        }
+    }
+}
