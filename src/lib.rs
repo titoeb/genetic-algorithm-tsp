39d911b722ae815e3f2d25eae367c6c39c7f8658
titoeb <tim.toebrock@gmail.com>
2022-01-13 07:26:58 +0000

    Added documentation to the overall library via `lib.rs`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
+//! # Genetic algorithms for solving TSPs.
+//!
+//! This crates contains utitlities to run genetic algorithms and solve Traveling Salesman Problems.
+
+/// Represent a distance Matrix as a Vec<Vec<f64>>.
 pub mod distance_mat;
+/// The `population`-module contains the main class of this crate which is the `Population`-class that contains
+/// your current subset of solutions and with which you can evolve them.
 pub mod population;
+/// The `solution`-module contains the `Solution`-class, the individual element of the TSP that implements
+/// important methods like `crossover` or `mutate`.
 pub mod solution;
+/// The `subsequence`-module contains a helper function, `Subsequence` that gives you functionality to select elements
+/// before, in and after a subsequence of a Vector. It is used extensively in the `ordered_crossover`-function.
 mod subsequence;
+/// the `test-utils`-module contains utitlities for testing and include for example the construction of test-data
+/// or the comparison of specializied objects (like permutations).
 mod test_utils;
+/// The `utils`-module contains utility that are used throughout the rest of the code base. The underlying `ordered_crossover`-
+/// function is implemented here.
 mod utils;
